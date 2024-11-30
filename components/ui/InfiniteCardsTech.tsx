6980c491e58d7b248a0e671516ddd131e9de33ca
Jack Will <65641460+jackwill99@users.noreply.github.com>
2024-11-30 19:43:34 +0700

    fix: skills list key

--- components/ui/InfiniteCardsTech.tsx
+++ components/ui/InfiniteCardsTech.tsx
@@ -31,7 +31,10 @@ export const InfiniteMovingCardsTech = ({
           return (
             <Fragment key={index}>
               {[...items, ...items].map((item, id) => (
-                <span className=" inline-flex items-center gap-4 lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E] whitespace-nowrap">
+                <span
+                  key={id}
+                  className=" inline-flex items-center gap-4 lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E] whitespace-nowrap"
+                >
                   {item}
                 </span>
               ))}
